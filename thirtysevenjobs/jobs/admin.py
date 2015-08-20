5c7d35e3bbbcbd411285418a6db4dbb6c30d80d7
Alex Phelps <alexphelps@PRONTOLAP0028.local>
2015-08-20 16:02:40 +0700

    initialize first jobs view

--- thirtysevenjobs/jobs/admin.py
+++ thirtysevenjobs/jobs/admin.py
@@ -3,13 +3,11 @@ from django.contrib import admin
 # Register your models here.
 from .models import Job
 from .models import Category
-from .models import Location
 
 class JobAdmin(admin.ModelAdmin):
     list_display = ('job_title','job_company','job_category','job_location','job_created_date','job_status')
     list_filter = ['job_status']
-    search_fields = ['job_title']
+    search_fields = ['job_title','job_company__company_name','job_location']
 
 admin.site.register(Job, JobAdmin)
 admin.site.register(Category)
-admin.site.register(Location)
--- thirtysevenjobs/jobs/models.py
+++ thirtysevenjobs/jobs/models.py
@@ -6,16 +6,10 @@ class Category(models.Model):
     def __unicode__(self):
         return self.category_name
 
-class Location(models.Model):
-    location_name = models.CharField(max_length=100)
-    def __unicode__(self):
-        return self.location_name
-
-
 class Job(models.Model):
     job_category = models.ForeignKey(Category,default=0)
     job_company = models.ForeignKey('companies.Company',default=0)
-    job_location = models.ForeignKey(Location,default=0)
+    job_location = models.CharField(max_length=200)
     job_title = models.CharField(max_length=200)
     job_description = models.TextField()
     job_created_date = models.DateTimeField('date created',null=True)
