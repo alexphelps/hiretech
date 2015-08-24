2d0099436fd5759e22234f3c9ac470a260113937
Alex Phelps <alexphelps@PRONTOLAP0028.local>
2015-08-24 23:34:28 +0700

    setup flatpages

--- thirtysevenjobs/thirtysevenjobs/settings.py
+++ thirtysevenjobs/thirtysevenjobs/settings.py
@@ -53,6 +53,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'django.contrib.flatpages',
     'markdown_deux',
     'jobs',
     'companies',
@@ -115,6 +117,8 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+SITE_ID = 1
 
 MEDIA_ROOT = normpath(join(DJANGO_ROOT, 'media'))
 MEDIA_URL = '/media/'
