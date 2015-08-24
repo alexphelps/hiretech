1e393e015fd30a54880d714cbf945bf0a913292c
Alex Phelps <alexphelps@PRONTOLAP0028.local>
2015-08-24 16:32:53 +0700

    add test for company detail repsonse

--- thirtysevenjobs/companies/tests.py
+++ thirtysevenjobs/companies/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import Company
+from jobs.models import Job
 
 # Create your tests here.
+class CompanyDetailsViewTest(TestCase):
+    def test_job_details_response(self):
+        company = Company.objects.create(
+            company_name='Pronto',
+            company_logo='/media/logo.png',
+        )
+        url = '/companies/' + str(company.id) + '/'
+        response = self.client.get(url)
+        
