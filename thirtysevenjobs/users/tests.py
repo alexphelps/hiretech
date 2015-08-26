adf1f6697291e1ae3e59c4dd40a27f5a9c9fe364
Alex Phelps <alexphelps@PRONTOLAP0028.local>
2015-08-26 16:00:40 +0700

    add sign up page status test

--- thirtysevenjobs/users/tests.py
+++ thirtysevenjobs/users/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
+class SignupViewTest(TestCase):
+    def test_signup_response(self):
+        url = '/signup/'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code,200)
