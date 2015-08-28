a7bc781e93f8d9c47928fd9c1921c0442a693e7e
Alex Phelps <alexphelps@PRONTOLAP0028.local>
2015-08-28 09:19:08 +0700

    add floating form labels

--- thirtysevenjobs/static/javascript/main.js
+++ thirtysevenjobs/static/javascript/main.js
@@ -1,3 +1,6 @@
 jQuery(document).ready(function() {
   jQuery(".timeago").timeago();
+  $('.form-control').on('focus blur', function (e) {
+      $(this).parents('.form-group').toggleClass('focused', (e.type === 'focus' || this.value.length > 0));
+  }).trigger('blur');
 });
