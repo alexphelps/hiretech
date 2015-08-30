2773e86265f316eb0a4a0c61198d1a4076a42605
Alex Phelps <alexphelps@PRONTOLAP0028.local>
2015-08-30 11:50:16 +0700

    change companies to use class based views and moved urls under companies app

--- thirtysevenjobs/companies/urls.py
+++ thirtysevenjobs/companies/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import patterns,url
-from . import views
+from django.conf.urls import patterns, url, include
+from django.views.generic import ListView
+from .models import Company
+from . import views as company_views
 
 urlpatterns = [
-    url(r'^(?P<company_id>[0-9]+)/$', views.company_details, name='company_details'),
+    url(r'^(?P<company_id>[0-9]+)/$', company_views.CompanyDetails.as_view(), name='company_details'),
+    url(r'^(?P<company_id>[0-9]+)/edit$', company_views.CompanyEditView.as_view(), name='comapny_edit')
 ]
--- thirtysevenjobs/companies/views.py
+++ thirtysevenjobs/companies/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 from django.http import Http404
 from django.shortcuts import get_object_or_404,render
 from django.http import HttpResponse
@@ -8,13 +9,32 @@ from django.conf import settings
 from .models import Company
 from jobs.models import Job
 
-def company_details(request, company_id):
-    #get all open jobs
-    company = get_object_or_404(Company, pk=company_id)
-    company_job_list = Job.objects.filter(job_company=company)
-    template = loader.get_template('company_details.html')
-    context = RequestContext(request, {
-        'company_job_list': company_job_list,
-        'company': company,
-    })
-    return HttpResponse(template.render(context))
+class CompanyDetails(TemplateView):
+    template_name = 'company_details.html'
+    def get(self,request, **kwargs):
+        company_id = self.kwargs['company_id']
+        company = get_object_or_404(Company, pk=company_id)
+        company_job_list = Job.objects.filter(job_company=company)
+        context = {
+            'company_job_list': company_job_list,
+            'company': company,
+        }
+        return render(
+            request,
+            self.template_name,
+            context
+        )
+
+class CompanyEditView(TemplateView):
+    template_name = 'company_edit.html'
+    def get(self,request, **kwargs):
+        company_id = self.kwargs['company_id']
+        company = get_object_or_404(Company, pk=company_id)
+        context = {
+
+        }
+        return render(
+            request,
+            self.template_name,
+            context
+        )
