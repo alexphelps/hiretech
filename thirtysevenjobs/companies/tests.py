9b6c0c0a6ce5b737d185c6651f35a1e77397a94a
Alex Phelps <alexphelps@PRONTOLAP0028.local>
2015-08-31 10:30:57 +0700

    fix companies tests

--- thirtysevenjobs/companies/tests.py
+++ thirtysevenjobs/companies/tests.py
@@ -6,7 +6,7 @@ from jobs.models import Job, Category
 class CompanyDetailsViewTest(TestCase):
     def test_company_details_response(self):
         company = Company.objects.create(
-            company_name='Pronto',
+            company_name='Git Jobs',
             company_logo='/media/logo.png',
             company_url='http://python.com',
         )
@@ -17,7 +17,7 @@ class CompanyDetailsViewTest(TestCase):
     def test_company_details_shows(self):
         category = Category.objects.create(category_name='Python Developer')
         company = Company.objects.create(
-            company_name='Python Shop',
+            company_name='Git Jobs',
             company_logo='/media/logo.png',
             company_url='http://python.com',
         )
@@ -30,21 +30,18 @@ class CompanyDetailsViewTest(TestCase):
         )
         url = '/companies/' + str(company.id) + '/'
         response = self.client.get(url)
-        expected = '<img class="img-responsive img-rounded" '
-        expected += 'src="/media/logo.png" />'
-        expected += '<h5 class="m-t-10">Python Shop</h5>'
-        expected += '<p><a href="http://python.com" rel="nofollow">'
-        expected += 'http://python.com</a></p>'
+        expected = '<img class="img-thumbnail job-details-img" src="/media/logo.png">'
+        expected += '<h3>Git Jobs</h3><p>'
+        expected += '<a href="http://python.com" rel="nofollow">http://python.com</a></p>'
         self.assertContains(response,expected)
         expected = '<li class="list-group-item">'
         expected += '<div class="row"><div class="col-md-9 col-sm-9 col-xs-8">'
-        expected += '<img class="img-responsive pull-left m-r-10 '
-        expected += 'job-list-img" src="/media/logo.png">'
-        expected += '<h5><a href="/jobs/1/">Python Guy</a><br>'
-        expected += '<small>Owensboro, KY</small></h5></div>'
+        expected += '<img class="img-responsive pull-left m-r-10 job-list-img" '
+        expected += 'src="/media/logo.png"><h5><a href="/jobs/1/">Python Guy</a>'
+        expected += '<br><small>Owensboro, KY</small></h5></div>'
         expected += '<div class="col-md-3 col-sm-3 col-xs-4">'
-        expected += '<ul class="list-unstyled text-center m-t-10"><li>'
-        expected += '<a href="/jobs/1/"><span class="job-type full_time">'
-        expected += 'Full Time</span></a></li><li><small class="timeago" '
-        expected += 'title="">None</small></li></ul></div></div></li>'
+        expected += '<ul class="list-unstyled text-center m-t-10">'
+        expected += '<li><a href="/jobs/1/"><span class="job-type full_time">Full Time</span></a>'
+        expected += '</li><li><small class="timeago" title="">None</small>'
+        expected += '</li></ul></div></div></li>'
         self.assertContains(response,expected)
