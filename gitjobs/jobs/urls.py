a201f5be7c75e12eb0149c4bde09f8163b5c41ef
Alex Phelps <alex@prontomarketing.com>
2015-09-09 20:10:39 +0700

    add login required to job add form

--- gitjobs/jobs/urls.py
+++ gitjobs/jobs/urls.py
@@ -4,7 +4,7 @@ from . import views as job_views
 
 urlpatterns = [
     url(r'^$', job_views.JobIndex.as_view(), name='jobindex'),
-    url(r'^add/$', job_views.JobAddNew.as_view(), name='jobadd'),
+    url(r'^add/$', login_required(job_views.JobAddNew.as_view()), name='jobadd'),
     url(r'^(?P<job_id>[0-9]+)/$', job_views.JobDetails.as_view(), name='jobdetail'),
     url(r'^(?P<job_id>[0-9]+)/filled/$',login_required(job_views.JobMarkAsFilled.as_view()), name='job_filled'),
 ]
