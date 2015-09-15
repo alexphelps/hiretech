9060f3b5e6b8a1277fc570a0444de2d4ab19b17f
Alex Phelps <alex@prontomarketing.com>
2015-09-15 17:59:42 +0700

    add login required for user update views

--- gitjobs/users/urls.py
+++ gitjobs/users/urls.py
@@ -3,6 +3,6 @@ from django.contrib.auth.decorators import login_required
 from . import views as user_views
 
 urlpatterns = [
-    url(r'^settings/$', user_views.UserSettingsView.as_view(), name='user_settings'),
-    url(r'^update-password/$', user_views.PasswordUpdateView.as_view(), name='user_settings'),
+    url(r'^settings/$', login_required(user_views.UserSettingsView.as_view()), name='user_settings'),
+    url(r'^update-password/$', login_required(user_views.PasswordUpdateView.as_view()), name='user_settings'),
 ]
