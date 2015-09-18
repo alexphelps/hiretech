67b0853ee7ae1bbece0fad0a473039676520c8d2
Alex Phelps <alex@prontomarketing.com>
2015-09-18 21:39:03 +0700

    fix jobs tests

--- gitjobs/jobs/tests.py
+++ gitjobs/jobs/tests.py
@@ -1,9 +1,17 @@
+import os
+from django.conf import settings
+from django.core.files import File
+
 from django.test import TestCase
 from .models import Job
 from companies.models import Company
 
 # Create your tests here.
 class JobIndexViewTest(TestCase):
+    def setUp(self):
+        image_source = '/tests/testdata/test-logo.png'
+        self.image_path = os.path.join(settings.SITE_ROOT + image_source)
+
     def test_index_response(self):
         url = '/jobs/'
         response = self.client.get(url)
@@ -13,10 +21,10 @@ class JobIndexViewTest(TestCase):
         url = '/jobs/'
         company = Company.objects.create(
             company_name='Alex Test Company',
-            company_logo='/media/logo.png',
+            company_logo= File(open(self.image_path)),
             company_slug='alex-test-company'
         )
-        Job.objects.create(
+        job = Job.objects.create(
             job_company=company,
             job_location='Owensboro, KY',
             job_title='Python Guy',
@@ -26,18 +34,22 @@ class JobIndexViewTest(TestCase):
         response = self.client.get(url)
         expected = '<li class="list-group-item"><div class="row">'
         expected += '<div class="col-md-9 col-sm-9 col-xs-8">'
-        expected += '<img class="img-responsive img-rounded pull-left m-r-10 job-list-img" '
-        expected += 'src="/media/logo.png"><h5><a href="/jobs/3/">Python Guy</a>'
-        expected += '<br><small><a href="/companies/alex-test-company" class="text-muted">Alex Test Company</a>'
-        expected += ' - Owensboro, KY</small></h5></div><div class="col-md-3 col-sm-3 col-xs-4">'
-        expected += '<ul class="list-unstyled text-center m-t-10"><li><a href="/jobs/3/">'
-        expected += '<span class="job-type full_time">Full Time</span></a></li><li>'
+        expected += '<img class="img-responsive img-rounded pull-left m-r-10 '
+        expected += 'job-list-img" src="/media/'
+        expected += str(company.company_logo_thumb) + '">'
+        expected += '<h5><a href="/jobs/'+ str(job.id) +'/">Python Guy</a><br><small>'
+        expected += '<a href="/companies/alex-test-company" class="text-muted">'
+        expected += 'Alex Test Company</a> - Owensboro, KY</small></h5>'
+        expected += '</div><div class="col-md-3 col-sm-3 col-xs-4">'
+        expected += '<ul class="list-unstyled text-center m-t-10"><li>'
+        expected += '<a href="/jobs/'+ str(job.id) +'/"><span class="job-type full_time">'
+        expected += 'Full Time</span></a></li>'
         self.assertContains(response,expected)
 
     def test_job_details_response(self):
         company = Company.objects.create(
             company_name='Python Shop',
-            company_logo='/media/logo.png',
+            company_logo= File(open(self.image_path)),
             company_url='http://python.com',
         )
         job = Job.objects.create(
