db70e7f16f893357e3f8e62e836a8e49190cff75
Alex Phelps <alex@prontomarketing.com>
2015-09-26 16:11:44 +0700

    adjust environment variables

--- hiretech/hiretech/settings/production.py
+++ hiretech/hiretech/settings/production.py
@@ -18,10 +18,10 @@ ALLOWED_HOSTS = [
 DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'DB_NAME',
-            'USER': 'DB_USER',
-            'PASSWORD': 'DB_PASSWORD',
-            'HOST': 'DB_HOST',
+            'NAME': os.environ.get('DB_NAME',''),
+            'USER': os.environ.get('DB_USER',''),
+            'PASSWORD': os.environ.get('DB_PASSWORD',''),
+            'HOST': os.environ.get('DB_HOST',''),
             'PORT': '',
         }
     }
