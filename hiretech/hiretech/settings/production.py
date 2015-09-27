29b13fc944b08c178b3fc16029928dad7215928a
Alex Phelps <alex@prontomarketing.com>
2015-09-27 19:47:27 +0700

    remove cloudfront for now

--- hiretech/hiretech/settings/production.py
+++ hiretech/hiretech/settings/production.py
@@ -12,8 +12,8 @@ ALLOWED_HOSTS = [
     '*'
 ]
 
-MEDIA_URL = '//d39h1855s3pz3w.cloudfront.net/media/'
-STATIC_URL = '//d39h1855s3pz3w.cloudfront.net/static/'
+#MEDIA_URL = '//d39h1855s3pz3w.cloudfront.net/media/'
+#STATIC_URL = '//d39h1855s3pz3w.cloudfront.net/static/'
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
