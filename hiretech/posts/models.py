a6180180293f3f5f6a83bfe93579eb51d27b966d
Alex Phelps <alex@prontomarketing.com>
2015-09-29 20:56:23 +0700

    add auto slug for posts

--- hiretech/posts/models.py
+++ hiretech/posts/models.py
@@ -6,15 +6,24 @@ from django.template.defaultfilters import slugify
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=100, unique=True)
-    slug = models.SlugField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True,blank=True)
     body = models.TextField()
     published_date = models.DateField(auto_now=True,null=True)
     category = models.ForeignKey('posts.Category')
     featured_image = models.ImageField(upload_to=settings.MEDIA_ROOT,default='')
 
+    def __unicode__(self):
+        return self.title
+
+    def save(self,**kwargs):
+        if self.title and not self.slug:
+            self.slug = slugify(self.title)
+        return super(Category, self).save(**kwargs)
+
+
 class Category(models.Model):
     title = models.CharField(max_length=100)
-    slug = models.SlugField(max_length=100)
+    slug = models.SlugField(max_length=100,unique=True,blank=True)
 
     def __unicode__(self):
         return self.title
