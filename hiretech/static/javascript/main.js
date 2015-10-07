0eadbeae9ce8f991be76e2034be79bb686d2ca7c
Alex Phelps <alex@prontomarketing.com>
2015-10-07 19:27:50 +0700

    refine dashboard and cleanup js

--- hiretech/static/javascript/main.js
+++ hiretech/static/javascript/main.js
@@ -3,10 +3,15 @@ jQuery(document).ready(function() {
   $('.form-control.floating-label').on('focus blur', function (e) {
       $(this).parents('.form-group').toggleClass('focused', (e.type === 'focus' || this.value.length > 0));
   }).trigger('blur');
-  //token tagging
-  $(".timeago").timeago();
-});
 
-$('.tokenfield').tokenfield({
-  inputType:'text',
+  $(".timeago").timeago();
+
+  //auto remove alerts with auto-close class
+  setTimeout(function() {
+    $(".alert.auto-close").alert('close');
+  }, 2000);
+
+  $('.tokenfield').tokenfield({
+    inputType:'text',
+  });
 });
