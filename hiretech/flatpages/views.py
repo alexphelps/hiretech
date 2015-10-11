cb039e1f4dd024fb486c9a17493f4f58f8151ea4
Alex Phelps <alex@prontomarketing.com>
2015-10-11 15:33:19 +0700

    add post feed to homepage and refine blog index

--- hiretech/flatpages/views.py
+++ hiretech/flatpages/views.py
@@ -7,6 +7,7 @@ from django.template import RequestContext, loader
 from django.conf import settings
 from search.forms import CustomSearchForm
 from jobs.models import Job
+from posts.models import Post
 
 class HomepageView(TemplateView):
     template_name = 'index.html'
@@ -14,10 +15,12 @@ class HomepageView(TemplateView):
         form = CustomSearchForm(request.GET, searchqueryset=None)
         user = request.user
         recent_five_jobs_list = Job.objects.filter(job_status='published').order_by('-job_created_date')[:5]
+        recent_post_list = Post.objects.filter(status='published').order_by('-published_date')[:3]
         context = {
             'user':user,
             'form': form,
             'recent_five_jobs_list':recent_five_jobs_list,
+            'recent_post_list':recent_post_list,
         }
         return render(
             request,
